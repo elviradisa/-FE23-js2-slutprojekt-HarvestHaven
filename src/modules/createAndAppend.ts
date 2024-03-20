92b995de1b9fdfbfadc96fc853ef84d2a4c14306
Amandaanderssn <tindraamanda@hotmail.se>
2024-03-20 12:13:25 +0100

    create and append function

---
+++ src/modules/createAndAppend.ts
@@ -0,0 +1,9 @@
+export function createAndAppend(parent: HTMLDivElement, elementName: string, textContent: string): HTMLElement {
+
+    const element = document.createElement(elementName);
+    if (elementName != 'div' && elementName != 'img') element.innerText = textContent;
+
+    parent.append(element);
+
+    return element;
+};
