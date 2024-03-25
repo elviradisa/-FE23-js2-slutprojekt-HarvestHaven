574f3b3ce7c93e4dea16a3b912e438317f2347e3
jesperanderson <150336326+jesperanderson@users.noreply.github.com>
2024-03-25 13:47:45 +0100

    Nya uppdateringar

---
+++ src/modules/forum.ts
@@ -0,0 +1,90 @@
+import { post } from "./fetch.ts";
+
+// Funktion för att skapa ett inlägg
+async function createPost(event: Event): Promise<void> {
+  event.preventDefault();
+
+  const postTitle = (document.getElementById("postTitle") as HTMLInputElement)
+    ?.value;
+  const postContent = (
+    document.getElementById("postContent") as HTMLTextAreaElement
+  )?.value;
+
+  if (postTitle && postContent) {
+    try {
+      // Skicka det nya inlägget till Firebase
+      await post({ postTitle, postContent });
+      // Återställ formuläret
+      (document.getElementById("postTitle") as HTMLInputElement).value = "";
+      (document.getElementById("postContent") as HTMLTextAreaElement).value =
+        "";
+      // Ger en uppdaterad lista
+      updatePostList();
+    } catch (error) {
+      console.error("Error creating post:", error);
+    }
+
+    console.log(postTitle);
+    console.log(postContent);
+  }
+}
+
+// Uppdatera inläggslistan
+async function updatePostList(): Promise<void> {
+  try {
+    const posts = await getPosts();
+    const postsList = document.getElementById("postsList");
+
+    if (postsList) {
+      postsList.innerHTML = ""; // Rensa tidigare inlägg
+
+      for (const postId in posts.forum1[0].posts) {
+        const post = posts.forum1[0].posts[postId];
+
+        if (post) {
+          const postElement = document.createElement("div");
+          postElement.classList.add("post");
+          
+
+          const postTitle = post.postTitle;
+          const postContent = post.postContent;
+
+          postElement.innerHTML = `
+          <div id="postsList"><div>
+              <img src=""/>
+              <h4 id="userID">${postId}</h4>
+            </div>
+            <div>
+              <h5>${postTitle}</h5>
+              <p>${postContent}</p>
+            </div>
+            <div>
+              <button onclick="commentOnPost('${postTitle}')">Kommentera</button></div>`;
+
+          postsList.appendChild(postElement);
+          console.log(postId);
+          console.log(post);
+          console.log(postTitle);
+          console.log(postContent);
+        }
+      }
+    }
+  } catch (error) {
+    console.error("Error updating post list:", error);
+  }
+}
+
+// Lägg till för formulärets submit-händelse
+const postForm = document.getElementById("postForm");
+if (postForm) {
+  postForm.addEventListener("submit", createPost);
+}
+
+// Funktion för att hämta inlägg från Firebase
+async function getPosts(): Promise<any> {
+  const response = await fetch(
+    "https://slutprojekt-js2-socialmedia-default-rtdb.europe-west1.firebasedatabase.app/.json"
+  );
+  const data = await response.json();
+  return data;
+}
